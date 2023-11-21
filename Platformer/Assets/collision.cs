2e0bd2c93386235b870c8bc02065be6931593710
awesomecow23 <ripsgabriel@gmail.com>
2023-11-21 15:03:20 +1100

    Finished collisino code

--- Platformer/Assets/collision.cs
+++ Platformer/Assets/collision.cs
@@ -4,29 +4,18 @@ using UnityEngine;
 
 public class collision : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
-    void OnCollisionEnter(Collision collision)
+    void OnCollisionEnter2D(Collision2D collision)
     {
         //Check for a match with the specified name on any GameObject that collides with your GameObject
         if (collision.gameObject.name == "Spike")
         {
             //If the GameObject's name matches the one you suggest, output this message in the console
-            transform.position = new Vector2()
+            transform.position = new Vector2(-12, -3);
         }
 
         //Check for a match with the specific tag on any GameObject that collides with your GameObject
-        if (collision.gameObject.tag == "MyGameObjectTag")
+        if (collision.gameObject.tag == "Respawn")
         {
             //If the GameObject has the same tag as specified, output this message in the console
             Debug.Log("Do something else here");
