f7754b54102acb03c4a7af921767925b48544f0d
awesomecow23 <ripsgabriel@gmail.com>
2023-11-22 20:31:25 +1100

    Scene resets when hitting spikes

--- Platformer/Assets/collision.cs
+++ Platformer/Assets/collision.cs
@@ -1,17 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class collision : MonoBehaviour
 {
 
     void OnCollisionEnter2D(Collision2D collision)
     {
+
         //Check for a match with the specified name on any GameObject that collides with your GameObject
         if (collision.gameObject.name == "Spike")
         {
             //If the GameObject's name matches the one you suggest, output this message in the console
-            transform.position = new Vector2(-12, -3);
+            string currentSceneName = SceneManager.GetActiveScene().name;
+            SceneManager.LoadScene(currentSceneName);
         }
 
         if (collision.gameObject.name == "Flag")
