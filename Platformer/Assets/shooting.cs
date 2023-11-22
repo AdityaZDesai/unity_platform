e6709bfd4b7b22a56389e3156594c973ee6cc037
AdityaZDesai <adityadesai753@gmail.com>
2023-11-23 00:29:45 +1100

    Added gun rotation

--- Platformer/Assets/shooting.cs
+++ Platformer/Assets/shooting.cs
@@ -8,6 +8,10 @@ public class shooting : MonoBehaviour
     public GameObject bullet;
     public Transform gunTransform; // Reference to the transform of the gun.
 
+    public Transform gun_pivot;
+
+    // Maximum distance for full rotation speed
+
     // Start is called before the first frame update
     void Start()
     {
@@ -17,8 +21,31 @@ public class shooting : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        // Get the mouse position in screen coordinates
+        Vector3 mousePos = Input.mousePosition;
+        
+        // Convert the mouse position to a point in the game world
+        mousePos = Camera.main.ScreenToWorldPoint(new Vector3(mousePos.x, mousePos.y, 10.0f)); // 10.0f is the distance from the camera
+
+        // Calculate the direction from the gun to the mouse position
+        Vector3 direction = mousePos - gun_pivot.position;
+        direction.z = 0; // Make sure the gun doesn't rotate in the z-axis
+
+        // Calculate the angle in radians
+        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+
+        // Calculate the rotation speed based on the distance
+        float distance = direction.magnitude;
+        float rotationSpeed = baseRotationSpeed * (1.0f - Mathf.Clamp01(distance / maxDistance));
+
+        // Rotate the gun towards the mouse position smoothly
+        Quaternion rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+        transform.rotation = Quaternion.Slerp(transform.rotation, rotation, rotationSpeed * Time.deltaTime);
+    
+
         if (Input.GetMouseButtonDown(0))
-        {
+        {   
+
             Quaternion currentRotation = gunTransform.rotation;
 
             // Add 270 degrees to the current rotation
